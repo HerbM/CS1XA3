63097a413d72c0ed2f81ace6f2543000d8080838
Hariesh Jayanthan <jayanthh@mills.cas.mcmaster.ca>
2018-02-20 16:47:43 -0500

    changed

--- ProjectAnalyze.sh
+++ ProjectAnalyze.sh
@@ -1,12 +1,13 @@
 #Checks if local repo is up to date with the remote repo
-git remote show origin
-
+git remote show origin > tmpCheckDate.txt
+if [grep "up to date" tmpCheckDate.txt]
+then echo "Up to Date"
+else echo "Not Up to Date" 
+fi
 
 # uncommited changes in a file changes.log. Note, this removes previous content of changes.log 
-git diff < changes.log
+git diff > changes.log
 
-# pastChanges contains all 
-git diff << pastChanges.log
 
 #Puts each Line from every file containing #TODO in a todo.log
 cat $(grep -l -r "#TODO" ) > todo.log
