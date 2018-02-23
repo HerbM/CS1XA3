0f9684697a8ac78c3c8ff9b1dd704f514d753be0
Hariesh Jayanthan <jayanthh@mills.cas.mcmaster.ca>
2018-02-22 23:56:24 -0500

    update

--- Assign1/ProjectAnalyze.sh
+++ Assign1/ProjectAnalyze.sh
@@ -1,4 +1,4 @@
-#Checks if local repo is up to date with the remote repo
+#hecks if local repo is up to date with the remote repo
 if git remote show origin | grep -q "up to date"
 then echo "Up to Date"
 else echo "Not Up to Date" 
@@ -41,30 +41,3 @@ else
 echo "Alright I wont clone anything."
 fi
 fi
-
-
-=======
->>>>>>> d7a3a0bf46780e223d639c6f2961c3046726d047
-
-#finds  haskell error
-
-if [ "find -name "*.hs" | wc -l" = "0" ] 
-then find -name "*.hs" |xargs ghc -fno-code > error.log
-else
-echo "There are no Haskell programs"
-echo "" > error.log
-fi
-
-#find -name "*.hs" |xargs ghc -fno-code > error.log
-
-#Asks for git clone
-echo "Would you like me to clone a repo? [Y,n]"
-read input
-if [[ $input == "Y" || $input == "y" ]]; then
-        echo "Please enter the repo"
-	read input2
-	git clone $input2
-else
-        echo "Okay I wont clone anything"
-fi
-
